d8154372f2201f5123a5c00abe819e9f97f7dbca
Sophie <sophiedodsworth@hotmail.com>
2023-01-13 19:56:39 +0000

    began adding questions to js file

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+
+    {
+        title: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<scripting>",
+            "<js>",
+            "<javascript",
+            "<script>"
+        ],
+        answer: "<script>"
+    },
+
+    {
+        title: "Where is the correct place to insert a JavaScript?",
+        choices: [
+            "The <body> section",
+            "The <head> section",
+            "Both the <head> section and the <body> section are correct",
+        ],
+        answer: "Both the <head> section and the <body> section are correct"
+    },
+
+    {
+        title: "Which of the following is an advantage of using JavaScript?",
+        choices: ["Less server interaction",
+            "Immediate feedback to the visitors",
+            "Increased interactivity",
+            "All of the above"
+        ],
+        answer: "alerts"
+    },
+    
+];

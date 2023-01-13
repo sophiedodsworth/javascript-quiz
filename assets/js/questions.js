b75d046f5425ee2006e2a931bbdb67a3f0c26987
Sophie <sophiedodsworth@hotmail.com>
2023-01-13 20:20:08 +0000

    completed questions and amended variable names

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,33 +1,56 @@
 var questions = [
 
     {
-        title: "Inside which HTML element do we put the JavaScript?",
-        choices: ["<scripting>",
+        questionTitle: "Inside which HTML element do we put the JavaScript?",
+        questionChoices: ["<scripting>",
             "<js>",
             "<javascript",
             "<script>"
         ],
-        answer: "<script>"
+        questionAnswer: "<script>"
     },
 
     {
-        title: "Where is the correct place to insert a JavaScript?",
-        choices: [
+        questionTitle: "Where is the correct place to insert a JavaScript?",
+        questionChoices: [
             "The <body> section",
             "The <head> section",
             "Both the <head> section and the <body> section are correct",
         ],
-        answer: "Both the <head> section and the <body> section are correct"
+        questionAnswer: "Both the <head> section and the <body> section are correct"
     },
 
     {
-        title: "Which of the following is an advantage of using JavaScript?",
-        choices: ["Less server interaction",
+        questionTitle: "Which of the following is an advantage of using JavaScript?",
+        questionChoices: [
+            "Less server interaction",
             "Immediate feedback to the visitors",
             "Increased interactivity",
             "All of the above"
         ],
-        answer: "alerts"
+        questionAnswer: "alerts"
     },
-    
+
+    {
+        questionTitle: "Which of the following is a valid type of function javascript supports?",
+        questionChoices: [
+            "named function",
+            "anonymous function",
+            "Both of the above",
+            "None of the above"
+        ],
+        questionAnswer: "Both of the above"
+    },
+
+    {
+        questionTitle: "Which of the following function of Array object adds and/or removes elements from an array?",
+        questionChoices: [
+            "toSource()",
+            "sort()",
+            "splice()",
+            "unshift()"
+        ],
+        questionAnswer: "splice()"
+    },
+
 ];

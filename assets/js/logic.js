bccf1cacedbd9ad1953e0c6847ca88de6a99f625
Sophie <sophiedodsworth@hotmail.com>
2023-01-14 16:24:04 +0000

    completed the logic file inluding question select, end of quiz & saving score

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,4 @@
-// itentifying the DOM elements
+// identifying the DOM elements
 
 var questionsEl = document.querySelector("#questions");
 var timerEl = document.querySelector("#time");
@@ -29,20 +29,20 @@ function startQuiz() {
 
   // starts the timer when user begins quiz
 
-  timerId = setInterval(clockTick, 1000);
+  timerId = setInterval(theTimer, 1000);
 
   // displays the time to the user
 
   timerEl.textContent = time;
 
-  // calling the function
+  // calling the function to load questions once the user starts the quiz
 
-  getQuestion();
+  loadQuestions();
 }
 
 // declaring function to start the questios
 
-function getQuestion() {
+function loadQuestions() {
 
   // declaring variable to select the question object from the created array
 
@@ -72,7 +72,7 @@ function getQuestion() {
     choiceNode.textContent = i + 1 + ". " + choice;
 
     // click event listener
-    choiceNode.onclick = questionClick;
+    choiceNode.onclick = questionSelect;
 
     // executing it to display for the user
 
@@ -81,7 +81,7 @@ function getQuestion() {
   });
 }
 
-function questionClick() {
+function questionSelect() {
 
   // check if user guessed wrong
 
@@ -117,26 +117,105 @@ function questionClick() {
   }, 1000);
 
   // moves the user along to the next question in the quiz
+
   currentQuestionIndex++;
 
   // checks the time 
 
   if (currentQuestionIndex === questions.length) {
-    quizEnd();
+    endQuiz();
 
   } else {
 
-    getQuestion();
+// calling function to load the questions again
 
+    loadQuestions();
+
+  }
+}
+
+function endQuiz() {
+
+  // the time stops at the end of the quiz
+
+  clearInterval(timerId);
+
+  // moves to the end screen
+
+  var endScreenEl = document.getElementById("end-screen");
+  endScreenEl.removeAttribute("class");
+
+  // show the user their score
+
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = time;
+
+  // hide the quiz questions 
+
+  questionsEl.setAttribute("class", "hide");
+}
+
+function theTimer() {
+
+  // the time is up to date
+
+  time--;
+  timerEl.textContent = time;
+
+  // if statement to check if the user taking the quiz has no time left
+
+  if (time <= 0) {
+    endQuiz();
+  }
+
+}
+
+// declaring function that saves the user's score
+
+function saveScore() {
+
+  var initials = initialsEl.value.trim();
+
+  if (initials !== "") {
+
+    // collects any saved scores and if there's none then it goes to an empty array
+
+    var highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // creates a new score 
+
+    var newScore = {
+      score: time,
+      initials: initials
+    };
+
+    // saves the score 
+
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+    // goes to the score page
+
+    window.location.href = "score.html";
+  }
+}
+
+// declaring function for when the user hits enter
+
+function userEnter(event) {
+
+  if (event.key === "Enter") {
+    saveScore();
   }
 }
 
 // user to submit initials and it saves their score
 
-submitBtn.onclick = saveHighscore;
+submitBtn.onclick = saveScore;
 
 // begin the quiz
 
 startBtn.onclick = startQuiz;
 
-initialsEl.onkeyup = checkForEnter;
+initialsEl.onkeyup = userEnter;

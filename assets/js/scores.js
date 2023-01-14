6642948227a3151aca29e763856a817176c4473f
Sophie <sophiedodsworth@hotmail.com>
2023-01-14 16:54:51 +0000

    started code in the scores js file

---
+++ assets/js/scores.js
@@ -0,0 +1,25 @@
+// declaring the function to print the scores
+
+function printHighscores() {
+
+    // takes the scores that are saved and if there aren't any then it creates an empty array
+
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    // when scores are displayed, this lists them in order like a typical scoreboard 
+
+    highscores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function (score) {
+
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + " - " + score.score;
+
+        // show the scores
+
+        var olEl = document.getElementById("highscores");
+        olEl.appendChild(liTag);
+    });
+}
